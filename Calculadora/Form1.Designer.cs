8daa6ef0916cf1204cb3ff74b5c80f27e42ab5de
CainMacias <cain.maciasb@gmail.com>
2026-01-30 15:52:34 -0600

    Se agrega boton reset al formulario de calculadora y se pueden hacer sumas tambien

--- Calculadora/Form1.Designer.cs
+++ Calculadora/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Calculadora
 {
-    partial class Form1
+    partial class Calculadora
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,18 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
+            textVarA = new TextBox();
+            variableA = new Label();
+            label1 = new Label();
+            textVarB = new TextBox();
+            BtCalcular = new Button();
+            label2 = new Label();
+            textResultado = new TextBox();
+            BtReset = new Button();
             SuspendLayout();
             // 
-            // Form1
+            // textVarA
+            // 
+            textVarA.Location = new Point(78, 51);
+            textVarA.Name = "textVarA";
+            textVarA.Size = new Size(278, 27);
+            textVarA.TabIndex = 0;
+            textVarA.TextChanged += textBox1_TextChanged;
+            // 
+            // variableA
+            // 
+            variableA.AutoSize = true;
+            variableA.Location = new Point(78, 28);
+            variableA.Name = "variableA";
+            variableA.Size = new Size(76, 20);
+            variableA.TabIndex = 1;
+            variableA.Text = "variable A";
+            variableA.Click += variableA_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(78, 102);
+            label1.Name = "label1";
+            label1.Size = new Size(75, 20);
+            label1.TabIndex = 3;
+            label1.Text = "variable B";
+            // 
+            // textVarB
+            // 
+            textVarB.Location = new Point(78, 125);
+            textVarB.Name = "textVarB";
+            textVarB.Size = new Size(278, 27);
+            textVarB.TabIndex = 2;
+            // 
+            // BtCalcular
+            // 
+            BtCalcular.Location = new Point(500, 51);
+            BtCalcular.Name = "BtCalcular";
+            BtCalcular.Size = new Size(94, 29);
+            BtCalcular.TabIndex = 4;
+            BtCalcular.Text = "CALCULAR";
+            BtCalcular.UseVisualStyleBackColor = true;
+            BtCalcular.Click += TnCalcular_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(78, 210);
+            label2.Name = "label2";
+            label2.Size = new Size(75, 20);
+            label2.TabIndex = 6;
+            label2.Text = "Resultado";
+            // 
+            // textResultado
+            // 
+            textResultado.Location = new Point(78, 233);
+            textResultado.Name = "textResultado";
+            textResultado.ReadOnly = true;
+            textResultado.Size = new Size(278, 27);
+            textResultado.TabIndex = 5;
+            // 
+            // BtReset
+            // 
+            BtReset.Location = new Point(500, 102);
+            BtReset.Name = "BtReset";
+            BtReset.Size = new Size(94, 29);
+            BtReset.TabIndex = 7;
+            BtReset.Text = "RESET";
+            BtReset.UseVisualStyleBackColor = true;
+            BtReset.Click += BtReset_Click;
+            // 
+            // Calculadora
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(842, 507);
-            Name = "Form1";
-            Text = "Form1";
+            Controls.Add(BtReset);
+            Controls.Add(label2);
+            Controls.Add(textResultado);
+            Controls.Add(BtCalcular);
+            Controls.Add(label1);
+            Controls.Add(textVarB);
+            Controls.Add(variableA);
+            Controls.Add(textVarA);
+            Name = "Calculadora";
+            Text = "Calculadora";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textVarA;
+        private Label variableA;
+        private Label label1;
+        private TextBox textVarB;
+        private Button BtCalcular;
+        private Label label2;
+        private TextBox textResultado;
+        private Button BtReset;
     }
 }
--- Calculadora/Form1.cs
+++ Calculadora/Form1.cs
@@ -1,10 +1,50 @@
 namespace Calculadora
 {
-    public partial class Form1 : Form
+    public partial class Calculadora : Form
     {
-        public Form1()
+        public Calculadora()
         {
             InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void variableA_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TnCalcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int a = 0, b = 0, resultado = 0;
+
+                a = Convert.ToInt32(textVarA.Text);
+                b = Convert.ToInt32(textVarB.Text);
+
+                resultado = a + b;
+
+                MessageBox.Show("El resultado de la suma es: " + resultado.ToString(), "Sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                textResultado.Text = resultado.ToString();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrió un error: " + MessageBoxIcon.Warning);
+
+
+            }
+        }
+
+        private void BtReset_Click(object sender, EventArgs e)
+        {
+            textVarA.Text = "";
+            textVarB.Clear();
         }
     }
 }
